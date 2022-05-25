e5b9b131a1a45b7f0e8cf1c31680b23813748cba
DieBlxme <79899724+DieBlxme@users.noreply.github.com>
2022-05-25 20:43:01 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-basic.showAnimation(`
-. . . . .
-. . . . .
-. . # . #
-. . . . .
-. . . . .
-`)
+let pixel = 3
+
+while(true) {
+    input.onButtonPressed(Button.B, function () {
+        if(pixel == 5) {
+            return;
+        }else {
+            pixel = pixel  + 1
+            return;
+        }
+    })
+
+    input.onButtonPressed(Button.A, function () {
+        if(pixel == 1) {
+            return;
+        }else {
+            pixel = pixel  - 1
+            return;
+        }
+    })
+
+    basic.showNumber(pixel)
+}
+
