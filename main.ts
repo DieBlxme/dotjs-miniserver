574e84bc9adc06d2eca2b2a75a1890566472faa2
DieBlxme <79899724+DieBlxme@users.noreply.github.com>
2022-05-25 21:15:56 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
-let pixel = 3
+//timer with typescript running on micro controller (32-bit ARM Cortex M0 processor (16MHz) CPU) Not compiled!
+//Compile with Microsoft
 
-while(true) {
-    input.onButtonPressed(Button.B, function () {
-        if(pixel == 5) {
-            return;
-        }else {
-            pixel = pixel  + 1
-            return;
-        }
-    })
+let seconds = 0
 
-    input.onButtonPressed(Button.A, function () {
-        if(pixel == 1) {
-            return;
-        }else {
-            pixel = pixel  - 1
-            return;
-        }
-    })
+basic.setLedColor(Colors.Green)
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    seconds = seconds + 1;
+    basic.showNumber(seconds)
+})
+input.onButtonPressed(Button.A, function () {
+    startTimer()
+})
 
-    basic.showNumber(pixel)
+function startTimer() {
+    basic.setLedColor(Colors.Red)
+    while(seconds > 0) {
+        basic.pause(1000)
+            basic.clearScreen()
+        seconds = seconds - 1;
+        basic.showNumber(seconds)
+    }
+    basic.turnRgbLedOff()
+    music.startMelody(music.builtInMelody(Melodies.Wedding))
 }
-
