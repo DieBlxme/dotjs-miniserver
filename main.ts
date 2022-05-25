63410450e76e25e6fcb529d63cb8c15414f44e74
DieBlxme <79899724+DieBlxme@users.noreply.github.com>
2022-05-25 22:37:53 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //timer with typescript running on micro controller (32-bit ARM Cortex M0 processor (16MHz) CPU) Not compiled!
-//Compile with Microsoft
+//Compile with Microsoft MakeCode (.hex)
 
 let seconds = 0
 
